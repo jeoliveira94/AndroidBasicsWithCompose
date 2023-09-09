5a190f1df7c25b12a969fbf2e7d2e09938fb4dd5
José Oliveira <eduardo.jeso@gmail.com>
2023-09-09 10:55:55 -0300

    Compose Article

---
+++ app/src/main/java/br/com/jeoliveira/playground/ComposeArticle.kt
@@ -0,0 +1,49 @@
+package br.com.jeoliveira.playground
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ComposeArticle(modifier: Modifier = Modifier){
+    Column {
+        Image(
+            painter = painterResource(R.drawable.bg_compose_background),
+            contentDescription = null,
+        )
+        Text(
+            text = stringResource(R.string.compose_article_header),
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = stringResource(R.string.compose_article_intro),
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp)
+        )
+        Text(
+            text = "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app's UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI's construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name.",
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(16.dp)
+
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewComposeArticle() {
+    ComposeArticle()
+}
